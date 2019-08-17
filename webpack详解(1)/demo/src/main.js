ee6289a76291d30f387dba17693d22ef9d80eee9
liao <mail@liaozhibin.com>
2019-08-18 03:00:20 +0800

    blog

--- webpack详解(1)/demo/src/main.js
+++ webpack详解(1)/demo/src/main.js
@@ -1,7 +1,11 @@
 // src/main.js
 import { say } from './say';
+import './style.css';
+
+const button = document.getElementById('btn');
+button.addEventListener('click', handlerClick);
 
 function handlerClick() {
     console.log('button clicked!');
-    say('Hello, Webpack!');
+    say('Hello, Webpack! Are you OK?');
 }
--- webpack详解(1)/demo/webpack.config.js
+++ webpack详解(1)/demo/webpack.config.js
@@ -1,10 +1,36 @@
 // webpack.config.js
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/main.js',
     output: {
         path: path.resolve(__dirname, './dist'),
         filename: 'bundler.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.html$/,
+                use: ['html-loader']
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif)$/,
+                use: ['file-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ],
+    devServer: {
+        contentBase: './dist',
+        port: 8000
     }
 }
